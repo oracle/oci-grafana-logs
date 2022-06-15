04190ab480070f314914941691a802c9ca4ce60a
Nagaraj Narayanaswamy <nagaraj.narayanaswamy@oracle.com>
2022-06-15 11:35:20 -0400

    update build script to use mage.

--- build.sh
+++ build.sh
@@ -9,42 +9,8 @@ fi
 rm -rf ./oci-logs-datasource
 ./node_modules/.bin/grunt
 
-# build go
-
-POST=''
-GOOS=''
-
-OS="`uname`"
-case $OS in
-  'Linux')
-      POST='_linux_amd64'
-      GOOS="linux"
-    ;;
-  'Darwin')
-      POST='_darwin_amd64'
-      GOOS="darwin"
-    ;;
-  'AIX') ;;
-  *) ;;
-esac
-
-# go mod vendor
-
-# if [[ ! -d ./vendor ]]; then
-#   echo "dependencies not installed try running | go mod vendor didn't work as expected"
-#   exit 1
-# fi
-
-echo "building go binary"
-
-# For debugger
-#  GOOS=$GOOS go build -o ./dist/oci-logs-plugin$POST -gcflags="all=-N -l"
-
-# For release
-GOOS=$GOOS go build -o ./dist/oci-logs-plugin$POST
-GOOS=linux go build -o ./dist/oci-logs-plugin_linux_amd64
-GOOS=windows GOARCH=amd64 go build -o ./dist/oci-logs-plugin_windows_amd64.exe
-grafana-toolkit plugin:sign
+mage --debug -v
+#grafana-toolkit plugin:sign
 mv ./dist ./oci-logs-datasource
 tar cvf plugin.tar ./oci-logs-datasource
 zip -r oci-logs-datasource ./oci-logs-datasource
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/grafana/grafana-plugin-sdk-go v0.126.0
+	github.com/magefile/mage v1.11.0
 	github.com/oracle/oci-go-sdk v24.3.0+incompatible
 	github.com/pkg/errors v0.9.1
 )
