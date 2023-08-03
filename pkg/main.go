83a50b74c976286c28e4482d45c4bfd377280217
Tejas Shah <tejas.t.shah@oracle.com>
2023-08-03 13:44:24 +0100

    REplia branch of the metrics plugin

---
+++ pkg/main.go
@@ -0,0 +1,19 @@
+s	package main
+
+import (
+	"os"
+
+	"github.com/grafana/grafana-plugin-sdk-go/backend"
+	"github.com/grafana/grafana-plugin-sdk-go/backend/datasource"
+
+	"github.com/oracle/oci-grafana-metrics/pkg/plugin"
+)
+
+const OCI_PLUGIN_ID = "oci-logs-datasource"
+
+func main() {
+	if err := datasource.Manage(OCI_PLUGIN_ID, plugin.NewOCIDatasource, datasource.ManageOpts{}); err != nil {
+		backend.Logger.Error(err.Error())
+		os.Exit(1)
+	}
+}
